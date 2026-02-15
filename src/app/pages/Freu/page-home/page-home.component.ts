05e12ebc0ce510498cebf8ef3f313733e713466c
Freu21 <nikiniklas4@gmail.com>
2026-02-15 15:22:35 +0100

    feat: home page erstellt

---
+++ src/app/pages/Freu/page-home/page-home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-home',
+  imports: [],
+  templateUrl: './page-home.component.html',
+  styleUrl: './page-home.component.scss'
+})
+export class PageHomeComponent {
+
+}
